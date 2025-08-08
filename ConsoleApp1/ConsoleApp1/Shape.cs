740e13cda4981e79ebbba3fedd8e82e0cb4514d8
Gabe <gkaddo@st.ug.edu.gh>
2025-08-08 17:29:31 +0000

    Added the Shape abstract class with the GetArea method.

---
+++ ConsoleApp1/ConsoleApp1/Shape.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+      abstract class Shape
+    {
+        public abstract void GetArea();
+       
+    
+     }
+
+     
+    
+}
