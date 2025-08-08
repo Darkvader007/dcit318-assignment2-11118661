5f00db461238905ddf4b5e72123ccc86180a71e4
Gabe <gkaddo@st.ug.edu.gh>
2025-08-08 21:13:40 +0000

    Added the interface and classes required.

---
+++ ConsoleApp1/ConsoleApp1/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    
+    public interface Imovable
+    {
+        void Move();
+    } 
+    
+     class Car : Imovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    class Bicycle : Imovable 
+    {
+        public void Move()
+        { 
+         Console.WriteLine("Bicycle is moving");
+        }
+    }
+}
